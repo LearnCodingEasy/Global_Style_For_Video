8d0220d7c352a3f1a68f5e123400db6b7cb6dc5a
Hossam Rashad <learncodingeasy@yahoo.com>
2026-08-29 23:59:37 +0300

    29/8/2026

--- project_docs/docs/.vuepress/.temp/pages/index.html.js
+++ project_docs/docs/.vuepress/.temp/pages/index.html.js
@@ -14,3 +14,18 @@ if (import.meta.hot) {
     __VUE_HMR_RUNTIME__.updatePageData(data)
   })
 }
+y@yahoo.com\",\"author\":\"Hossam Rashad\",\"message\":\"project_docs\"}]},\"filePathRelative\":\"README.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
